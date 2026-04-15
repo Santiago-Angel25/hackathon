e46958ac55288dda294614088bb94aa5fe965c03
SantiagoAngel <santiago062225@gmail.com>
2026-04-15 13:12:21 -0500

    esste sii

---
+++ src/main/java/com/example/hackathon/repository/UbicacionRepository.java
@@ -0,0 +1,7 @@
+package com.example.hackathon.repository;
+
+import com.example.hackathon.model.Ubicacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UbicacionRepository extends JpaRepository<Ubicacion, Long> {
+}
