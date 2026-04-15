98735d4aaec9250eee87450b449c345084b81548
SantiagoAngel <santiago062225@gmail.com>
2026-04-15 12:13:49 -0500

    hoall

--- src/main/java/com/example/hackathon/model/TipoDonacion.java
+++ src/main/java/com/example/hackathon/model/TipoDonacion.java
@@ -1,14 +1,23 @@
 package com.example.hackathon.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "tipos_donacion")
 public class TipoDonacion {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    private String nombre;
+
+    @Id
+    private Long id;
+    @Column(name = "id_tipo")
+    private Integer idTipo;
+
+    @Column(name = "id_donador")
+    private Integer idDonador;
+
+    @Column(name = "id_ubicacion")
+    private Integer idUbicacion;
 }
